5fef973c050088c701fe74bb41ef32be05f89e3b
Alan Tavares <alan_tj@hotmail.com.br>
2022-02-02 15:36:53 -0300

    escopo inicial

---
+++ ApiLojaStore/Controllers/VendaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiLojaStore.Controllers
+{
+    public class VendaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ApiLojaStore/Model/User.cs
@@ -0,0 +1,6 @@
+﻿namespace ApiLojaStore.Model
+{
+    public class User
+    {
+    }
+}
