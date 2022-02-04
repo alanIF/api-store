aa87ac84e08943947a8236f37ca6f837b9fcfb78
Alan Tavares <alan_tj@hotmail.com.br>
2022-02-04 08:03:06 -0300

    User API Finished, Add new method view all API Controller, Tested all API

--- ApiLojaStore/Controllers/UserController.cs
+++ ApiLojaStore/Controllers/UserController.cs
@@ -1,12 +1,72 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiLojaStore.Context;
+using ApiLojaStore.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiLojaStore.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class UserController : Controller
     {
-        public IActionResult Index()
+        private readonly BDContext _context;
+        public UserController(BDContext context)
         {
-            return View();
+            _context = context;
+
+        }
+        [HttpGet("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var users = await _context.Users.ToListAsync();
+            if (users is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(users);
+        }
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create([Bind("Name, Email, Password")] User user)
+        {
+            _context.Add(user);
+            await _context.SaveChangesAsync();
+
+
+            return Ok(user);
+        }
+        [HttpPatch("Update")]
+        public async Task<IActionResult> Update(int id, [Bind("Name, Email, Password")] User user)
+        {
+            if (id != user.Id)
+            {
+                return NotFound();
+            }
+
+
+            try
+            {
+                _context.Update(user);
+                await _context.SaveChangesAsync();
+                return Ok(user);
+
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+
+            }
+            return NotFound();
+
+        }
+        [HttpDelete("Delete")]
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return Ok("Usuario excluída com sucesso");
+
         }
     }
 }
