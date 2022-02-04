6cc1c32bf41a388076649f2629f937c7c2254f39
Alan Tavares <alan_tj@hotmail.com.br>
2022-02-04 08:16:53 -0300

    Auth

---
+++ ApiLojaStore/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using ApiLojaStore.Context;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ApiLojaStore.Controllers
+{ 
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly BDContext _context;
+        public AuthController(BDContext context)
+        {
+            _context = context;
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(string Email, string Password)
+        {
+            var user = _context.Users.Where(u => u.Email == Email && u.Password == Password).ToList();
+            if (user.Any())
+            {
+               
+
+                return Ok(user);
+
+            }
+
+            return Ok("Usuario não logado");
+        }
+
+
+    [HttpGet]
+    public async Task<IActionResult> Logout()
+    {
+            return Ok("Deslogado");
+
+        }
+    }
+}
